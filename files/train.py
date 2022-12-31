1d3bd80f1fecbb252aac387bcee712c128053f6e
FathiMahdi <electronics_forever@outlook.com>
2023-01-01 04:36:47 +0800

    modified:   data/dataset.csv 	modified:   train.py

--- files/train.py
+++ files/train.py
@@ -1,12 +1,22 @@
 from sklearn.ensemble import RandomForestClassifier
 import emlearn
 import pandas as pd
+import numpy as np
 
+dataset = 'data/dataset.csv'
 lib_name = 'model'
 
 # get the data form CSV files
-X_train = []
-Y_train  = []
+data_frame = pd.read_csv(dataset)
+#data_frame = data_frame.to_numpy()
+print(data_frame)
+X_train = data_frame.iloc[:,0]
+Y_train = data_frame.iloc[:,1]
+X_train =  X_train.to_numpy()
+X_train = X_train.reshape(-1, 1)
+Y_train =  Y_train.to_numpy()
+print('X: ',X_train)
+print('Y: ',Y_train)
 # Model Fit
 estimator = RandomForestClassifier(n_estimators=10, max_depth=10)
 estimator.fit(X_train, Y_train)
