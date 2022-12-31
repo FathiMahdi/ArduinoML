2fd51a8f0eed93f5731281d6fc1b0868dbf02f9e
FathiMahdi <electronics_forever@outlook.com>
2023-01-01 03:56:37 +0800

    modified:   ../train.py

--- files/train.py
+++ files/train.py
@@ -2,9 +2,11 @@ from sklearn.ensemble import RandomForestClassifier
 import emlearn
 import pandas as pd
 
+lib_name = 'model'
+
 # get the data form CSV files
-
-
+X_train = []
+Y_train  = []
 # Model Fit
 estimator = RandomForestClassifier(n_estimators=10, max_depth=10)
 estimator.fit(X_train, Y_train)
@@ -12,4 +14,4 @@ estimator.fit(X_train, Y_train)
 
 # convert model to c code
 cmodel = emlearn.convert(estimator, method='inline')
-cmodel.save(file='sonar.h')
+cmodel.save(file='{}.h'.format(lib_name))
